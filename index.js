6fba06df550736db87221c398108345596614961
JoaquinAngelino <joacoangelino@gmail.com>
2022-11-11 00:33:19 -0300

    node modules deleted

--- index.js
+++ index.js
@@ -4,14 +4,9 @@ import { pagination } from './js/pagination.js';
 import { searchListener } from './js/searchListener.js';
 import { setCount } from './js/setCount.js';
 
-var NAME = ""
-
 
 const products = await fetchProduct()
 const categories = await fetchCategory()
-
-console.log(products)
-console.log(categories)
 
 addContent(products, categories)
 setCount(products.count)
